4c6478520846a3d91bd566d2e253c5378e205905
BARTEKK23 <164915721+Bartosz8291@users.noreply.github.com>
2025-10-02 20:21:53 +0200

    Update translate.js

--- api/translate.js
+++ api/translate.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       return;
     }
 
-    const response = await fetch('https://libretranslate.de/translate', {
+    const response = await fetch('https://libretranslate.com/translate', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
